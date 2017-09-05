3ece4ae5afa475353f3030ab058b86e47bfc35d0
JeNeSaisToujoursPas <pschneider@de.ibm.com>
2017-09-05 12:56:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,12 +17,12 @@ provider "ibm" {
 ##############################################################################
 # IBM SSH Key: For connecting to VMs
 ##############################################################################
-resource "ibm_compute_ssh_key" "ssh_key" {
-  label = "${var.key_label}"
-  notes = "${var.key_note}"
-  # Public key, so this is completely safe
-  public_key = "${var.public_key}"
-}
+#resource "ibm_compute_ssh_key" "ssh_key" {
+#  label = "${var.key_label}"
+#  notes = "${var.key_note}"
+#  # Public key, so this is completely safe
+#  public_key = "${var.public_key}"
+#}
 
 ##############################################################################
 # Variables
@@ -39,19 +39,50 @@ variable slapikey {
 variable datacenter {
   description = "The datacenter to create resources in."
 }
-variable public_key {
-  description = "Your public SSH key material."
-}
-variable key_label {
-  description = "A label for the SSH key that gets created."
-}
-variable key_note {
-  description = "A note for the SSH key that gets created."
+#variable public_key {
+#  description = "Your public SSH key material."
+#}
+#variable key_label {
+#  description = "A label for the SSH key that gets created."
+#}
+#variable key_note {
+#  description = "A note for the SSH key that gets created."
+#}
+variable hostname {
+  description = "Hostname of instance"
 }
 
 ##############################################################################
+# Compute Instance
+##############################################################################
+resource "softlayer_virtual_guest" "centos_small_virtual_guest" {
+  name = "${var.hostname}",
+	image = "CentOS_7_64"
+  domain = "cam.ibm.com",
+  region = "${var.datacenter}",
+  public_network_speed = 10,
+  hourly_billing = true,
+  private_network_only = false,
+  cpu = 1,
+  ram = 1024,
+  disks = [
+      "25",
+      "10",
+      "20"
+  ],
+  user_data = "{\\\"value\\\":\\\"newvalue\\\"}",
+  dedicated_acct_host_only = true,
+  local_disk = false,
+  ssh_keys = [
+      "${softlayer_ssh_key.patricks_public_key.id}"
+  ]
+}
+  
+  
+  
+##############################################################################
 # Outputs
 ##############################################################################
-output "ssh_key_id" {
-  value = "${ibm_compute_ssh_key.ssh_key.id}"
-}
+#output "ssh_key_id" {
+#  value = "${ibm_compute_ssh_key.ssh_key.id}"
+#}
